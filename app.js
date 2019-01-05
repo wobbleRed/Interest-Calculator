6a608261b1c00d1145e1b076de764f497067dacd
Derek Allred <derekmallred@gmail.com>
2019-01-05 09:45:42 -0700

    loan calculator

--- app.js
+++ app.js
@@ -1,1 +1,66 @@
-console.log('connected')
+document.querySelector('#loan-form').addEventListener('submit', function (e) {
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'block';
+  setTimeout(calculateResults, 2000);
+  e.preventDefault();
+})
+
+document.querySelector('#clear-inputs').addEventListener('click', clearInputs)
+
+function calculateResults() {
+
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  const principle = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+  // calculate monthly pament
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principle * x * calculatedInterest) / (x - 1);
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principle).toFixed(2);
+    document.getElementById('results').style.display = 'block';
+    document.getElementById('loading').style.display = 'none';
+  } else {
+    showError('ENTER VALID INPUTS')
+  }
+}
+
+function clearInputs(e) {
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'none';
+
+  amount.value = '';
+  interest.value = '';
+  years.value = '';
+  e.preventDefault()
+}
+
+function showError(error) {
+  const errorDiv = document.createElement('div');
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'none';
+
+  errorDiv.className = 'alert alert-danger';
+  errorDiv.appendChild(document.createTextNode(error));
+  card.insertBefore(errorDiv, heading);
+  setTimeout(clearError, 3000);
+}
+
+function clearError() {
+  document.querySelector('.alert').remove();
+}
